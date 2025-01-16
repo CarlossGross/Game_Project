b5979e0ae5ff37b48ec46ab1b1cfaff5cf4a66cd
CarlosGross <cae.gross@hotmail.com>
2025-01-16 13:30:33 -0400

    Added FPS maximizer - 60 FPS

--- main.py
+++ main.py
@@ -3,19 +3,34 @@ from constants import *
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+fps_maximizer = pygame.time.Clock()
+dt = 0
+
 
 def main ():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+# infinite loop
     i=0
     while i == 0:
+
+# make quit button on window work
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+    
+# make screen black    
         screen.fill((0,0,0))
+
+# make screen update on every iteration of infinite loop
         pygame.display.flip()
+
+# maximize fps on 60FPS
+        dt = fps_maximizer.tick(60)/1000
+        print(dt)
+
 
 
 if __name__ == "__main__":
