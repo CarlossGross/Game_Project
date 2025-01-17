67a0e7d663231e097cbe157f18d699892c26a581
CarlosGross <cae.gross@hotmail.com>
2025-01-17 12:18:24 -0400

    Created dawable and updatable Groups

--- main.py
+++ main.py
@@ -6,15 +6,23 @@ from player import *
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 fps_maximizer = pygame.time.Clock()
+dt = 0
+
+# creating containers and assigning elements on containers
+updatabe = pygame.sprite.Group()
+drawable = pygame.sprite.Group()
+Player.containers = (updatabe, drawable)
 
 #create the player instance
 playr = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
 def main ():
-    dt = 0
+    global dt
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
 
 # infinite loop
     i=0
@@ -28,11 +36,13 @@ def main ():
 # make screen black    
         screen.fill((0,0,0))
 
-# draw the player
-        playr.draw(screen)
+# draw the drawable container items
+        for item in drawable:
+             item.draw(screen)
 
-# update player position
-        playr.update(dt)
+# update the updatable container items
+        for item in updatabe:
+             item.update(dt)
 
 # make screen update on every iteration of infinite loop - must be the final of the loop
         pygame.display.flip()
